b0f62f31305069d7f73ee8242a4124bb98b65d3e
Kyle <kyleyhw@gmail.com>
2025-01-17 17:29:00 +0000

    made game human playable

---
+++ play_game.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from rules import game
+
+# game format: 5 letter word followed by 5 letter string, each char in string contains 'b' or 'y' or 'g' for black or yellow or green respectively
+
+words = np.genfromtxt('data/wordle_allowed_guesses_unsorted.txt', dtype=str)
+
+# fix seed
+np.random.seed(seed=0)
+
+index = np.random.randint(low=0, high=len(words), size=1, dtype=int)[0]
+
+solution = words[index]
+
+game = game(solution=solution)
+tries = 0
+guess = '-----'
+
+while guess != solution:
+    tries +=1
+    guess = input('enter your next guess: ')
+    print(game.enter(guess=guess))
+
+print('congratulations! it took ' + str(tries) + ' guesses')
--- rules.py
+++ rules.py
@@ -1,8 +1,4 @@
-import numpy as np
-
 from funcs import split, count_occurrences
-
-words = np.genfromtxt('data/wordle_allowed_guesses_unsorted.txt', dtype=str)
 
 # game format: 5 letter word followed by 5 letter string, each char in string contains 'b' or 'y' or 'g' for black or yellow or green respectively
 
@@ -28,8 +24,3 @@ class game:
             # the = in the >= is because checked_letters includes the current letter, for more elegant initialization
                 colors[i] = 'y'
         return colors
-
-test_game = game('prose')
-print(test_game.enter('crane'))
-print(test_game.enter('soily'))
-print(test_game.enter('prose'))
